3a16f0a14a51c09da3d6167097b74a6c83d0fb57
yani <yanazhuchok@gmail.com>
2025-02-20 13:48:49 +0300

    fix

--- configurator/src/main/kotlin/com/dulfinne/configurator/repository/IconRepository.kt
+++ configurator/src/main/kotlin/com/dulfinne/configurator/repository/IconRepository.kt
@@ -4,7 +4,9 @@ import com.dulfinne.configurator.entity.Icon
 import org.springframework.data.domain.Page
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Repository
 
+@Repository
 interface IconRepository : JpaRepository<Icon, Long> {
     fun findAllByType(type: Int, pageable: Pageable): Page<Icon>
     fun findByName(name: String): Icon?
--- configurator/src/main/kotlin/com/dulfinne/configurator/service/impl/IconServiceImpl.kt
+++ configurator/src/main/kotlin/com/dulfinne/configurator/service/impl/IconServiceImpl.kt
@@ -9,14 +9,17 @@ import com.dulfinne.configurator.repository.IconRepository
 import com.dulfinne.configurator.service.IconService
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class IconServiceImpl(val iconRepository: IconRepository) : IconService {
+    @Transactional(readOnly = true)
     override fun getAllIcons(page: Int, size: Int): PaginatedResponse<IconResponse> {
         val icons = iconRepository.findAll(PageRequest.of(page, size))
         return icons.map { it.toResponse() }.toPaginatedResponse()
     }
 
+    @Transactional(readOnly = true)
     override fun getAllIconsByType(type: IconType, page: Int, size: Int): PaginatedResponse<IconResponse> {
         val icons = iconRepository.findAllByType(type.id, PageRequest.of(page, size))
         return icons.map { it.toResponse() }.toPaginatedResponse()
